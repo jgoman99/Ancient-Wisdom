c0c00379b13bbc1a0c3f0f5f98480c4985564bc4
John <jgfriedman99@gmail.com>
2023-12-16 13:32:34 -0800

    working flask server

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+import re
+import numpy as np
+import pandas as pd
+from search_embeddings import search_embeddings
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def process_text():
+    user_input = request.form['user_input']
+    processed_text = your_processing_function(user_input)
+    return render_template('index.html', result=processed_text)
+
+def your_processing_function(text):
+    # Implement your processing logic here
+    return f'Processed: {str(search_embeddings(text))}'
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- search_embeddings.py
+++ search_embeddings.py
@@ -2,29 +2,32 @@
 import pandas as pd
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
-
 from create_embeddings import query
-
-user_query = "competing with yourself is the only competition you need"
-user_query_embedding = query(user_query)
-user_query_embedding = np.array(user_query_embedding).reshape(1,-1)
 
 embeddings_df = pd.read_csv("Embedded Data/quotes.csv")
 
-# Specify columns to exclude
-exclude_columns = ['author', 'quote']
-
-# Select all columns except 'author' and 'quote'
-selected_columns = embeddings_df.drop(exclude_columns, axis=1)
-text_columns = embeddings_df.filter(exclude_columns, axis=1)
-# Calculate cosine similarity
-cosine_sim = cosine_similarity(selected_columns,user_query_embedding)
-
-# Find the three biggest elements
-three_biggest = np.argsort(cosine_sim, axis=0)[-3:].flatten()
-
-results = text_columns.iloc[three_biggest]
+user_query = 'expertise'
+def search_embeddings(user_query):
+    user_query_embedding = query(user_query)
+    user_query_embedding = np.array(user_query_embedding).reshape(1,-1)
 
 
-for idx, result in results.iloc[::-1].iterrows():
-    print(f"Similarity: {round(cosine_sim[idx][0],2)}; {result.author} said: '{result.quote}'\n\n")
+
+    # Specify columns to exclude
+    exclude_columns = ['author', 'quote']
+
+    # Select all columns except 'author' and 'quote'
+    selected_columns = embeddings_df.drop(exclude_columns, axis=1)
+    text_columns = embeddings_df.filter(exclude_columns, axis=1)
+    # Calculate cosine similarity
+    cosine_sim = cosine_similarity(selected_columns,user_query_embedding)
+
+    # Find the three biggest elements
+    three_biggest = np.argsort(cosine_sim, axis=0)[-3:].flatten()
+
+    results = text_columns.iloc[three_biggest]
+
+    formatted_text = ''
+    for idx, result in results.iloc[::-1].iterrows():
+        formatted_text += f"Similarity: {round(cosine_sim[idx][0],2)}; {result.author} said: '{result.quote}'\n\n"
+    return formatted_text
